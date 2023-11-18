297faedc5a3866a35f3aae57b92ef5deae40feef
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-18 15:21:11 +0100

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pracownik pracownik = new Pracownik("Tomasz", "Nowak", "IT", 6000);
+        Pracownik pracownik2 = new Pracownik("Anna", "Kowalska", "IT", 7000);
+        System.out.println( "Imię: " + pracownik.imie +
+                            "\nNazwisko: " + pracownik.nazwisko +
+                            "\nDział: " + pracownik.dzial +
+                            "\nWynagrodzenie: " + pracownik.wyplata);
+
+        System.out.println( "Imię: " + pracownik2.imie +
+                "\nNazwisko: " + pracownik2.nazwisko +
+                "\nDział: " + pracownik2.dzial +
+                "\nWynagrodzenie: " + pracownik2.wyplata);
     }
 }
---
+++ src/main/java/org/example/Pracownik.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Pracownik {
+    public String imie;
+    public String nazwisko;
+    public String dzial;
+    public Integer wyplata;
+
+    public Pracownik(String imie, String nazwisko, String dzial, Integer wyplata) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.dzial = dzial;
+        this.wyplata = wyplata;
+    }
+}
