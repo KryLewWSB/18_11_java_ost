857f2b36f3233c12b0fb75ac87918290f7469b1a
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-18 15:33:34 +0100

    3

---
+++ src/main/java/org/example/Oddzial.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public enum Oddzial {
+    GDANSK,
+    GDYNIA,
+    SOPOT,
+    KRAKOW,
+    WARSZAWA
+}
--- src/main/java/org/example/Pracownik.java
+++ src/main/java/org/example/Pracownik.java
@@ -1,15 +1,23 @@
 package org.example;
 
 public class Pracownik {
-    public String imie;
-    public String nazwisko;
-    public String dzial;
-    public Integer wyplata;
+    private String imie;
+    private String nazwisko;
+    private String dzial;
+    private Integer wyplata;
+    private Oddzial oddzial;
 
-    public Pracownik(String imie, String nazwisko, String dzial, Integer wyplata) {
+    public Pracownik(String imie, String nazwisko, String dzial, Integer wyplata, Oddzial odzial) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.dzial = dzial;
         this.wyplata = wyplata;
+        this.oddzial = odzial;
+    }
+    public void wyswietlDanePracownika(){
+        System.out.println( "Imię: " + this.imie +
+                "\nNazwisko: " + this.nazwisko +
+                "\nDział: " + this.dzial +
+                "\nWynagrodzenie: " + this.wyplata);
     }
 }
